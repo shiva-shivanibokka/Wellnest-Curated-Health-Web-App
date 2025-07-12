a7086246b0db687d0eacbdfc1edcbc1a8ed9dd0f
kevinhu03 <kevin.03.hu@gmail.com>
2025-07-12 04:10:35 -0700

    Started frontend and backend connection
    Added create user endpoint
    Started adding pages to django

--- application/vertical_prototype/backend_project/core/views.py
+++ application/vertical_prototype/backend_project/core/views.py
@@ -1,11 +1,17 @@
 from rest_framework import generics
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserCreateSerializer
+from django.shortcuts import render
+from django.conf import settings
+import os
 
-class UserListCreateView(generics.ListCreateAPIView):
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
+
+class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+def index(request):
+    return render(request, 'home.html')
