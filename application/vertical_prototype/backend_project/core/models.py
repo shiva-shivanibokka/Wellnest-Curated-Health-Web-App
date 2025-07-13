47ba6211c920f8c62b1718a513e7c63ba43a7013
Hamed Emari <Hamedemari@gmail.com>
2025-07-12 23:50:11 -0700

    updatedUserTablewithKev

--- application/vertical_prototype/backend_project/core/models.py
+++ application/vertical_prototype/backend_project/core/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    gender= models.CharField(max_length=100)
     encrypted_password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
